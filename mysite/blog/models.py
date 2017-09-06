0665a0ac2687ae64620de9a32f97184cbc43cfb7
Nik Yerra <nik_yerra@Niks-MacBook-Pro.local>
2017-09-06 01:40:24 -0500

    Files Added

---
+++ mysite/blog/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length = 200)
+    text =  models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(null = True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def get_absolute_url(self):
+        """
+        After post is completed, take the user to the post_deatil
+        page with the primary key of the post that was just created
+        """
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.post',related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default =  timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
